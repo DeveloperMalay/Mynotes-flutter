5b2c618bde1ece4ea14c3481ac6a5bfdaad65b22
Malay <malay13pandit@gmail.com>
2022-06-28 16:27:09 +0530

    step8

--- lib/views/Login_View.dart
+++ lib/views/Login_View.dart
@@ -34,73 +34,82 @@ class _Login_ViewState extends State<Login_View> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Login")),
-      body: Column(
-        children: [
-          TextField(
-            controller: _email,
-            enableSuggestions: false,
-            autocorrect: false,
-            keyboardType: TextInputType.emailAddress,
-            decoration:
-                const InputDecoration(hintText: 'Enter your email here'),
-          ),
-          TextField(
-            controller: _password,
-            obscureText: true,
-            enableSuggestions: false,
-            autocorrect: false,
-            decoration:
-                const InputDecoration(hintText: 'Enter your password here'),
-          ),
-          TextButton(
-            onPressed: () async {
-              final email = _email.text;
-              final password = _password.text;
-              try {
-                await FirebaseAuth.instance.signInWithEmailAndPassword(
-                  email: email,
-                  password: password,
-                );
-                Navigator.of(context).pushNamedAndRemoveUntil(
-                  notesroute,
-                  (route) => false,
-                );
-              } on FirebaseAuthException catch (e) {
-                if (e.code == 'user-not-found') {
-                  await showErrorDialog(
-                    context,
-                    'User not found',
+      body: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _email,
+              enableSuggestions: false,
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration:
+                  const InputDecoration(hintText: 'Enter your email here'),
+            ),
+            TextField(
+              controller: _password,
+              obscureText: true,
+              enableSuggestions: false,
+              autocorrect: false,
+              decoration:
+                  const InputDecoration(hintText: 'Enter your password here'),
+            ),
+            TextButton(
+              onPressed: () async {
+                final email = _email.text;
+                final password = _password.text;
+                try {
+                  await FirebaseAuth.instance.signInWithEmailAndPassword(
+                    email: email,
+                    password: password,
                   );
-                } else if (e.code == 'wrong-password') {
+                  final user = FirebaseAuth.instance.currentUser;
+                  if (user?.emailVerified ?? false) {
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      notesroute,
+                      (route) => false,
+                    );
+                  } else {
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      verifyEmailroute,
+                      (route) => false,
+                    );
+                  }
+                } on FirebaseAuthException catch (e) {
+                  if (e.code == 'user-not-found') {
+                    await showErrorDialog(
+                      context,
+                      'User not found',
+                    );
+                  } else if (e.code == 'wrong-password') {
+                    await showErrorDialog(
+                      context,
+                      'wrong password',
+                    );
+                  } else {
+                    await showErrorDialog(
+                      context,
+                      'Error:${e.code}',
+                    );
+                  }
+                } catch (e) {
                   await showErrorDialog(
                     context,
-                    'wrong password',
-                  );
-                } else {
-                  await showErrorDialog(
-                    context,
-                    'Error:${e.code}',
+                    e.toString(),
                   );
                 }
-              } catch (e) {
-                await showErrorDialog(
-                  context,
-                  e.toString(),
-                );
-              }
-            },
-            child: const Text('Login'),
-          ),
-          TextButton(
-              onPressed: () {
-                Navigator.of(context)
-                    .pushNamedAndRemoveUntil(registerroute, (route) => false);
               },
-              child: const Text("Don,t have a account? Register here "))
-        ],
+              child: const Text('Login'),
+            ),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .pushNamedAndRemoveUntil(registerroute, (route) => false);
+                },
+                child: const Text("Don,t have a account? Register here "))
+          ],
+        ),
       ),
     );
   }
 }
-
-
--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -16,28 +16,31 @@ class _VerifyEmailState extends State<VerifyEmail> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('verify Email ')),
-      body: Column(children: [
-        const Text(
-            "We've sent you an email verifivation.Please oprn it to verify your account "),
-        const Text("If you haven't recevied email yet, press the button below"),
-        TextButton(
+      body: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Column(children: [
+          const Text(
+              "We've sent you an email verifivation.Please oprn it to verify your account "),
+          const Text("If you haven't recevied email yet, press the button below"),
+          TextButton(
+              onPressed: () async {
+                final user = FirebaseAuth.instance.currentUser;
+                await user?.sendEmailVerification();
+                devtools.log('Email send');
+              },
+              child: const Text('Send Email Verification')),
+          TextButton(
             onPressed: () async {
-              final user = FirebaseAuth.instance.currentUser;
-              await user?.sendEmailVerification();
-              devtools.log('Email send');
+              await FirebaseAuth.instance.signOut();
+              Navigator.of(context).pushNamedAndRemoveUntil(
+                registerroute,
+                (route) => false,
+              );
             },
-            child: const Text('Send Email Verification')),
-        TextButton(
-          onPressed: () async {
-            await FirebaseAuth.instance.signOut();
-            Navigator.of(context).pushNamedAndRemoveUntil(
-              registerroute,
-              (route) => false,
-            );
-          },
-          child: const Text('Restart'),
-        )
-      ]),
+            child: const Text('Restart'),
+          )
+        ]),
+      ),
     );
   }
 }
