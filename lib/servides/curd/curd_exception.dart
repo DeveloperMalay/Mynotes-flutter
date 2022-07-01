a96e58e4d813556b833787ec6c1a03737333358a
Malay <malay13pandit@gmail.com>
2022-07-01 23:29:05 +0530

    step11

---
+++ lib/servides/curd/curd_exception.dart
@@ -0,0 +1,17 @@
+class DatabaseAlreadyOpenExceprion implements Exception {}
+
+class UnableToGetDocumentDirectory implements Exception {}
+
+class DatabaseNotOpen implements Exception {}
+
+class CouldNotdeleteUser implements Exception {}
+
+class UserAlreadyExits implements Exception {}
+
+class CouldNotFindUser implements Exception {}
+
+class CouldNotdeleteNote implements Exception {}
+
+class CouldNotFoundNote implements Exception {}
+
+class CouldNotUpdateNote implements Exception {}
